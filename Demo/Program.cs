f0c76cb6bf4f1e112040957360b192c92e7af661
fawazislam70@gmail.com <fawazislam70@gmail.com>
2024-07-29 22:41:00 +0100

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ArrayList
+            //ArrayList Numbers = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            ////Numbers.Add(1);
+            ////Numbers.Add(2);
+            ////Numbers.Add(3);
+            ////Numbers.Add(4);
+            ////Numbers.Add(5);
+            //Console.WriteLine("Count : " + Numbers.Count);
+            //Console.WriteLine("Capacity : " + Numbers.Capacity);
+
+            //Numbers.TrimToSize();
+            //Console.WriteLine("Count : " + Numbers.Count);
+            //Console.WriteLine("Capacity : " + Numbers.Capacity);
+
+            //for (int i = 0; i < Numbers.Count; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //} 
+            #endregion
+
+        }
+    }
+}
