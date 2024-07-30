7b19a7d0f7f4507764ee97b416643a69d7a839b6
fawazislam70@gmail.com <fawazislam70@gmail.com>
2024-07-30 11:02:09 +0100

    List

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -18,6 +18,16 @@ namespace Demo
 
             return Sum;
         }
+
+        static public int SumList(List<int> Numbers) {
+          int Sum = 0;
+            for (int i = 0; i < Numbers.Count; i++)
+            {
+                Sum += Numbers[i];
+            }
+            return Sum;
+
+        }
         
         static void Main(string[] args)
         {
@@ -44,6 +54,40 @@ namespace Demo
             #region NonGenric Cons
             //Console.WriteLine(Sum(Numbers)); 
             #endregion
+
+            #region Genric List
+            //List<int> Numbers = new List<int>();
+            //Console.WriteLine($"Count : {Numbers.Count}");
+            //Console.WriteLine($"Capacity : {Numbers.Capacity}");
+            //Numbers.Add(0);
+            //Numbers.Add(1);
+            //Numbers.AddRange([2, 6,4]);
+            //Console.WriteLine($"Count : {Numbers.Count}");
+            //Console.WriteLine($"Capacity : {Numbers.Capacity}");
+
+            //Numbers.TrimExcess();
+            //Console.WriteLine($"Count : {Numbers.Count}");
+            //Console.WriteLine($"Capacity : {Numbers.Capacity}");
+
+            //List<int> ints = new List<int>(5) { 1, 2, 3, 4 ,5};
+
+            //for (int i = 0; i < ints.Count; i++)
+            //{
+            //    Console.WriteLine(ints[i]);
+            //}
+
+            //Numbers.EnsureCapacity(21);
+            //for (int i = 6; i <= 21; i++)
+            //{
+            //    Numbers.Add(i);
+            //}
+            //Console.WriteLine($"Count : {Numbers.Count}");
+            //Console.WriteLine($"Capacity : {Numbers.Capacity}");
+
+            //Console.WriteLine(SumList(Numbers));
+
+            #endregion
+
 
         }
     }
