4a1c9283a783c155ce35590c90ca489659f552ec
fawazislam70@gmail.com <fawazislam70@gmail.com>
2024-07-30 20:06:57 +0100

    Question1

---
+++ Assignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        #region Q1
+        //1. Given an array  consists of  numbers with size N and number of queries,
+        //in each query you will be given an integer X,
+        //and you should print how many numbers in array that is greater than  X.
+
+
+        //string[] sizes = Console.ReadLine().Split(' ');
+
+        //int n = int.Parse(sizes[0]);
+        //int queriesNo = int.Parse(sizes[1]);
+
+        //string input = Console.ReadLine();
+
+        //string[] inputArray = input.Split(' ');
+        //int[] arr = new int[n];
+        //for (int i = 0; i < n; i++)
+        //{
+        //    arr[i] = int.Parse(inputArray[i]);
+        //}
+
+        //if (arr.Length != n)
+        //{
+        //    Console.WriteLine("The number of input  doesnt equal size");
+        //    return;
+        //}
+
+        //int[] queries = new int[queriesNo];
+        //for (int i = 0; i < queriesNo; i++)
+        //{
+        //    queries[i] = int.Parse(Console.ReadLine());
+        //}
+
+        //for (int i = 0; i < queriesNo; i++)
+        //{
+        //    int query = queries[i];
+        //    int count = 0;
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //        if (arr[j] > query)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    Console.WriteLine(count); 
+        //}
+        #endregion
+
+
+
+    }
+}
